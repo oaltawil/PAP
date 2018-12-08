d4c694e5c79987b9e84313f09ff4b7937c3b8c97
Osama Altawil <oaltawil@outlook.com>
2018-12-08 14:13:39 -0500

    Updated the NetSh commands

--- Scripts/EnableAzureMFA_NPSServer.ps1
+++ Scripts/EnableAzureMFA_NPSServer.ps1
@@ -41,19 +41,39 @@ netsh nps add registeredserver $existingDomainName $npsVmName
 # e. Create RADIUS clients for the 2 RD Gateway Servers
 #
 
-$primaryGatewayVmName, $secondaryGatewayVmName | New-NpsRadiusClient -Name $_  -Address $($_ + "." + $existingDomainName) -SharedSecret $sharedSecret
+$primaryGatewayVmName, $secondaryGatewayVmName | ForEach-Object {New-NpsRadiusClient -Name $_  -Address $($_ + "." + $existingDomainName) -SharedSecret $sharedSecret}
 
 #
 # f. Create a Network Policy to authorize valid connection requests
 #
-
-netsh nps add np name = "RDG_CAP" state = "enable" processingorder = "3" policysource = "0" conditionid = "0x1006" conditiondata = "0 00:00-24:00; 1 00:00-24:00; 2 00:00-24:00; 3 00:00-24:00; 4 00:00-24:00; 5 00:00-24:00; 6 00:00-24:00" profileid = "0x102f" advprofiletype = "IPFILTER_IPV4INFILTER" advprofiledata = "PERMIT"
-
 <#
+Network policy configuration:
+---------------------------------------------------------
+Name             = RDG_CAP
+State            = Enabled
+Processing order = 5
+Policy source    = 1
 
+Condition attributes:
+
+Name                                    Id          Value
+---------------------------------------------------------
+Condition0                              0x1006      "0 00:00-24:00; 1 00:00-24:00; 2 00:00-24:00; 3 00:00-24:00; 4 00:00-24:00; 5 00:00-24:00; 6 00:00-24:00"
+
+Profile attributes:
+
+Name                                    Id          Value
+---------------------------------------------------------
 NP-Allow-Dial-in                        0x100f      "TRUE"
 NP-Authentication-Type                  0x1009      "0x3" "0x9" "0x4" "0xa" "0x7"
 Framed-Protocol                         0x7         "0x1"
 Service-Type                            0x6         "0x2"
 
 #>
+
+$networkPolicyName = "RDG_CAP"
+
+netsh nps add np name = $networkPolicyName state = "enable" processingorder = "3" policysource = "1" conditionid = "0x1006" conditiondata = "0 00:00-24:00; 1 00:00-24:00; 2 00:00-24:00; 3 00:00-24:00; 4 00:00-24:00; 5 00:00-24:00; 6 00:00-24:00" profileid = "0x100f" profiledata = "TRUE" profileid = "0x7" profiledata = "0x1" profileid = "0x6" profiledata = "0x2"
+
+# The command below fails to configure the NP-Authentication-Type (with a Profile Id of 0x1009) with an array of values
+netsh nps set np name = "RDG_CAP" profileid = "0x1009" profiledata = "`"0x3`" `"0x9`" `"0x4`" `"0xa`" `"0x7`""
